d15a4fc2cf7c6fbcd07a4ca877661f56e971ac44
Fluidize <mbxiao1@gmail.com>
2023-08-17 18:34:31 -0500

    fixed bar

--- progress_bar.py
+++ progress_bar.py
@@ -3,9 +3,10 @@ from typing import Any
 from termcolor import colored
 
 class progress_bar():
-    def __init__(self, stop, start=0):
+    def __init__(self, stop, start=0, bar_length=50):
         self.count = start
         self.stop = stop
+        self.bar_length = bar_length
     
     def __iter__(self):
         return self
@@ -18,11 +19,16 @@ class progress_bar():
         
         elif self.count != self.stop:
             self.count += 1
-            percentage = '〙' + str(round(self.count * 100/self.stop, 2)) + '%'
+
+
+            percentage = round(self.count * 100/self.stop, 2)
+            bar_percentage = '〙' + str(percentage) + '%'
+            
             if round(self.count * 100/self.stop, 2) == 100:
-                percentage = colored(percentage, 'green')
-            bar = colored('〘','green') + colored('█','green')*self.count + "-" * (self.stop-self.count)
-            sys.stdout.write(f"{bar}{percentage} \r")
+                bar_percentage = colored(bar_percentage, 'green')
+            
+            bar = colored('〘','green') + colored('█','green')*round((percentage*(self.bar_length/(100/self.bar_length))/self.bar_length)) + "-" * (round(self.bar_length-percentage*(self.bar_length/(100/self.bar_length))/self.bar_length)) # god pleaseh elp me i dont know what this math is oh my god ??? wtf is this what how why what
+            sys.stdout.write(f"{bar}{bar_percentage} \r")
             sys.stdout.flush()
             return self.count
 
@@ -46,6 +52,6 @@ class animation():
             sys.stdout.write('[%s]\r' % (anim[self.count % 4]))
             sys.stdout.flush()
             return self.count
-    
+
 for x in progress_bar(50):
-    time.sleep(0.1)
+    time.sleep(0.05)
