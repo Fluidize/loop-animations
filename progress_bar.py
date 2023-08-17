03b5726390a8d2df4fa8d93d3e1cc1c9393c229d
Fluidize <mbxiao1@gmail.com>
2023-08-17 18:41:37 -0500

    better

--- progress_bar.py
+++ progress_bar.py
@@ -32,6 +32,33 @@ class progress_bar():
             sys.stdout.flush()
             return self.count
 
+class progress_bar_manual():
+    def __init__(self, total, bar_length=50):
+        self.count = 0
+        self.stop = total
+        self.bar_length = bar_length
+    
+    def update(self, added_count):
+        
+        if self.count == self.stop:
+            print('')
+            raise StopIteration
+        
+        elif self.count != self.stop:
+            self.count += added_count
+
+
+            percentage = round(self.count * 100/self.stop, 2)
+            bar_percentage = '〙' + str(percentage) + '%'
+            
+            if round(self.count * 100/self.stop, 2) == 100:
+                bar_percentage = colored(bar_percentage, 'green')
+            
+            bar = colored('〘','green') + colored('█','green')*round((percentage*(self.bar_length/(100/self.bar_length))/self.bar_length)) + "-" * (round(self.bar_length-percentage*(self.bar_length/(100/self.bar_length))/self.bar_length)) # god pleaseh elp me i dont know what this math is oh my god ??? wtf is this what how why what
+            sys.stdout.write(f"{bar}{bar_percentage} \r")
+            sys.stdout.flush()
+
+
 class animation():
     def __init__(self, stop, start=0):
         self.count = start
@@ -53,5 +80,3 @@ class animation():
             sys.stdout.flush()
             return self.count
 
-for x in progress_bar(50):
-    time.sleep(0.05)
